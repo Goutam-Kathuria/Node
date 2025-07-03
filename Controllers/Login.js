aeefdcf95ec181e805bc35ab93f2cfc1fa48e5d0
Goutam-Kathuria <goutam.kathuria@gmail.com>
2025-07-03 16:08:02 +0530

    Update Login.js

--- Controllers/Login.js
+++ Controllers/Login.js
@@ -447,28 +447,23 @@ const addMainCategory = async (req, res) => {
     let parsedFilters = [];
 
     if (filter) {
-      const rawFilters = JSON.parse(filter); 
+  const rawFilters = JSON.parse(filter); 
 
-      for (const f of rawFilters) {
-        const filterId = new ObjectId(f._id);
-        const filterDoc = await filterCollection.findOne({ _id: filterId });
+  for (const f of rawFilters) {
+    const filterId = new ObjectId(f._id);
+    const filterDoc = await filterCollection.findOne({ _id: filterId });
 
-        if (!filterDoc) {
-          console.log(`Filter not found for id: ${f._id}`);
-          continue;
-        }
+    parsedFilters.push({
+      _id: filterId,
+      Filter_name: filterDoc.Filter_name,
+      Filter: filterDoc.Filter.map(item => ({
+        _id: item._id,
+        name: item.name
+      }))
+    });
+  }
+}
 
-        // const selectedValues = filterDoc.Filter.map(item => ({
-        //   _id: new ObjectId(item._id),
-        //   name: item.name
-        // }));
-
-        parsedFilters.push({
-          _id: filterId,
-          Filter_name: filterDoc.Filter_name,
-        });
-      }
-    }
 
     const newCategory = {
       name,
@@ -488,7 +483,6 @@ const addMainCategory = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 
 
